5cad4a37192ee67f602a1664801b7dabd8bfa91d
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2017-11-28 12:56:40 -0600

    Solved all problems

--- database.sql
+++ database.sql
@@ -107,3 +107,41 @@ VALUES (1, 3, 0),
 (7, 2, 4),
 (6, 3, 88),
 (6, 4, 3);
+
+-- 1. Get all customers and their addresses.
+SELECT * FROM customers
+JOIN addresses ON customers.id = addresses.customer_id;
+
+--2. Get all orders and their line items.
+SELECT * FROM line_items
+JOIN orders ON orders.id = line_items.order_id;
+
+--3. Which warehouses have cheetos?
+SELECT * FROM products
+JOIN warehouse_product ON products.id = warehouse_product.product_id
+JOIN warehouse ON warehouse.id = warehouse_product.warehouse_id
+WHERE products.id = 5;
+
+--4. Which warehouses have diet pepsi?
+SELECT * FROM products
+JOIN warehouse_product ON products.id = warehouse_product.product_id
+JOIN warehouse ON warehouse.id = warehouse_product.warehouse_id
+WHERE products.id = 6;
+
+--5. Get the number of orders for each customer.
+SELECT customers.first_name, customers.last_name, COUNT(orders.id) FROM orders
+JOIN addresses ON orders.address_id = addresses.id
+JOIN customers ON addresses.customer_id = customers.id
+GROUP BY customers.id;
+
+--6. How many customers do we have?
+SELECT COUNT(customers.id) FROM customers;
+
+--7. How many products do we carry?
+SELECT COUNT(products.id) FROM products;
+
+--8. What is the total available on-hand quantity of diet pepsi?
+SELECT SUM(warehouse_product.on_hand) FROM products
+JOIN warehouse_product ON products.id = warehouse_product.product_id
+JOIN warehouse ON warehouse.id = warehouse_product.warehouse_id
+WHERE products.id = 6;
